7f1565bd9e633e2767858943858b7c4cb9543de3
Bryan <bryanstevenal@gmail.com>
2022-11-29 20:13:45 +0100

    Set Country type for Observable at PaisService

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
     providedIn: 'root'
@@ -13,12 +14,12 @@ export class PaisService {
     constructor(private http: HttpClient) { }
 
 
-    buscarPais(termino: string): Observable<any> {
+    buscarPais(termino: string): Observable<Country[]> {
         const url = `${this.apiUrl}/name/${termino}`;
 
         console.log("[PaisService] [buscarPais()] url: ", url);
 
-        return this.http.get(url);
+        return this.http.get<Country[]>(url);
         /*
         .pipe(
             // También podríamos retornar un Observable, p.e. un observable de array vacío
