de4d5f2fd5d388c5dea0573abf93de34e7f40f80
Bryan <bryanstevenal@gmail.com>
2022-11-29 19:28:56 +0100

    Use PaisService in order to call endpoint at PorPaisComponent

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
     selector: 'app-por-pais',
@@ -9,10 +10,25 @@ import { Component } from '@angular/core';
 export class PorPaisComponent {
 
     public termino: string = '';
+    public hayError: boolean = false;
 
-    constructor() { }
+    constructor(private paisService: PaisService) { }
 
     buscar() {
         console.log("[PaisModule] [PorPaisComponent] [buscar()] termino: ", this.termino);
+
+        this.hayError = false;
+
+        this.paisService
+            .buscarPais(this.termino)
+            .subscribe({
+                next: (respuesta) => {
+                    console.log(respuesta);
+                },
+                error: (err) => {
+                    this.hayError = true;
+                    console.log("Error: ", err);
+                }
+            });
     }
 }
