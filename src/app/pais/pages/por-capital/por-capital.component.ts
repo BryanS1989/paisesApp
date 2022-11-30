c7acddc62382c0591ff5bce8603ff15cf3355ccf
Bryan <bryanstevenal@gmail.com>
2022-11-30 21:23:56 +0100

    Create PorCapitalComponent

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
     selector: 'app-por-capital',
@@ -6,11 +8,36 @@ import { Component, OnInit } from '@angular/core';
     styles: [
     ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
 
-    constructor() { }
+    public paises: Country[] = [];
+    public termino: string = '';
+    public hayError: boolean = false;
+    public placeholder: string = "Buscar Capital...";
 
-    ngOnInit(): void {
+    constructor(private paisService: PaisService) { }
+
+    buscar(termino: string) {
+
+        console.log("[PaisModule] [PorCapitalComponent] [buscar()] termino: ", termino);
+
+        this.hayError = false;
+        this.termino = termino;
+
+        this.paisService
+            .buscarCapital(termino)
+            .subscribe({
+                next: (paises) => {
+                    console.log("[PaisModule] [PorCapitalComponent] [buscar()] paises: ", paises);
+                    this.paises = paises;
+                },
+                error: (err) => {
+                    console.log("[PaisModule] [PorCapitalComponent] [buscar()] Error: ", err);
+                    this.hayError = true;
+                    this.paises = [];
+                }
+            });
+
     }
 
 }
