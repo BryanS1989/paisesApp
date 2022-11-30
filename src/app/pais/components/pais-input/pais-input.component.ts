a46de3e67fc797a4facffc1485eb1efd4e51c3ae
Bryan <bryanstevenal@gmail.com>
2022-11-30 19:18:00 +0100

    Use debounce in order to emit a search value when user stops typing by 300ms

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,20 +1,51 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 
 @Component({
     selector: 'app-pais-input',
     templateUrl: './pais-input.component.html',
     styleUrls: []
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit {
 
-    @Output() onEnter: EventEmitter<string> = new EventEmitter();
+    @Output() onEnter: EventEmitter<string> = new EventEmitter();       // Will emit when user push enter
+    @Output() onDebounce: EventEmitter<string> = new EventEmitter();    // Will emit when user stops writting
 
+    public debouncer: Subject<string> = new Subject();  // Is like an Observable
     public termino: string = '';
+
+
+    /**
+     * NgOnInit will be excuted one time when the component was created and initialized
+     */
+    ngOnInit(): void {
+        console.log("[PaisModule] [PaisInputComponent] [ngOnInit()]");
+
+        this.debouncer
+            .pipe(
+                // Do not continue to subscribe since the observable (debouncer) stop emitting values by 300ms
+                debounceTime(300)
+            )
+            .subscribe((valor) => {
+                console.log("[PaisModule] [PaisInputComponent] [ngOnInit()] [debouncer] valor", valor);
+                this.onDebounce.emit(valor);
+            });
+    }
 
     buscar() {
         console.log("[PaisModule] [PaisInputComponent] [buscar()] termino: ", this.termino);
 
         this.onEnter.emit(this.termino);
+    }
+
+    teclaPresionada() {
+        console.log("[PaisModule] [PaisInputComponent] [teclaPresionada()]");
+
+        // Emit a value to debouncer
+        this.debouncer.next(this.termino);
+
     }
 
 }
