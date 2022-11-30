eb3bb39e085db0c5b3b6d080265741cfb8fb677f
Bryan <bryanstevenal@gmail.com>
2022-11-30 18:51:59 +0100

    Add output to PaisInputComponent

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-pais-input',
@@ -7,10 +7,14 @@ import { Component } from '@angular/core';
 })
 export class PaisInputComponent {
 
+    @Output() onEnter: EventEmitter<string> = new EventEmitter();
+
     public termino: string = '';
 
     buscar() {
         console.log("[PaisModule] [PaisInputComponent] [buscar()] termino: ", this.termino);
+
+        this.onEnter.emit(this.termino);
     }
 
 }
