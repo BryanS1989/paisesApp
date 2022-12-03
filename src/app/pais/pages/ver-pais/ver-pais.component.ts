d4299d16a64b5173d0a512afa7f3086c6c1f5494
Bryan <bryanstevenal@gmail.com>
2022-12-03 19:59:17 +0100

    At VerPaisComponent subscribe to changes at route url and search the country info by its alpha code

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
     selector: 'app-ver-pais',
@@ -8,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-    constructor() { }
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private paisService: PaisService
+    ) { }
 
     ngOnInit(): void {
+        // Nos subcribimos a los cambios de la ruta
+        this.activatedRoute.params
+            .subscribe(({ id }) => {    // Desestructurando params y quedandonos con la propiedad id
+                console.log("[PaisModule] [VerPaisComponent] [ngOnInit()] id: ", id);
+
+                this.paisService
+                    .getPaisPorAlpha(id)
+                    .subscribe((pais) => {
+                        console.log("[PaisModule] [VerPaisComponent] [ngOnInit()] pais: ", pais);
+                    });
+            });
+        /*
+            Sin desestructurar los params:
+            .subscribe((params) => {
+                console.log("[PaisModule] [VerPaisComponent] [ngOnInit()] params: ", params);
+            });
+        */
     }
 
 }
