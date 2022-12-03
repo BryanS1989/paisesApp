450ff41ca974db042dc513c60a9dd8a584465852
Bryan <bryanstevenal@gmail.com>
2022-12-03 20:06:14 +0100

    At VerPaisComponent subscribe to changes at route url and search the country info by its alpha code using RxJs operator SWITCHMAP

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
 
+import { switchMap } from 'rxjs/operators';     // Switchmap nos permite recibir un observable y retornar otro observable
+
 @Component({
     selector: 'app-ver-pais',
     templateUrl: './ver-pais.component.html',
@@ -18,6 +20,15 @@ export class VerPaisComponent implements OnInit {
     ngOnInit(): void {
         // Nos subcribimos a los cambios de la ruta
         this.activatedRoute.params
+            .pipe(  // dentro de pipe podemos especificar cualquier operador Rxjs para trabajar con el producto del observable
+                switchMap(({ id }) => this.paisService.getPaisPorAlpha(id))  // switchmap resibe el observable params y retorna otro observable
+            )
+            .subscribe((pais) => {
+                console.log("[PaisModule] [VerPaisComponent] [ngOnInit()] pais: ", pais);
+            });
+
+        /*
+        this.activatedRoute.params
             .subscribe(({ id }) => {    // Desestructurando params y quedandonos con la propiedad id
                 console.log("[PaisModule] [VerPaisComponent] [ngOnInit()] id: ", id);
 
@@ -27,6 +38,7 @@ export class VerPaisComponent implements OnInit {
                         console.log("[PaisModule] [VerPaisComponent] [ngOnInit()] pais: ", pais);
                     });
             });
+        */
         /*
             Sin desestructurar los params:
             .subscribe((params) => {
