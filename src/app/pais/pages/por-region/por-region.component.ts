75ec93f94b4a0dbb7c2107f5f68d824f55d71ef0
Bryan <bryanstevenal@gmail.com>
2022-12-04 11:46:40 +0100

    Implement PorRegionComponent TS

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
     selector: 'app-por-region',
@@ -6,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
     styles: [
     ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+    public regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+    public paises: Country[] = [];
+    public regionActiva: string = this.regiones[0];
 
     constructor() { }
 
-    ngOnInit(): void {
+    getClaseCss(region: string) {
+        console.log("[PaisModule] [PorRegionComponent] [getClaseCss()] región: ", region);
+        return (region === this.regionActiva) ? 'nav-link active' : 'nav-link';
+    }
+
+    activarRegion(region: string) {
+        console.log("[PaisModule] [PorRegionComponent] [activarRegion()] región: ", region);
+        this.regionActiva = region;
     }
 
 }
