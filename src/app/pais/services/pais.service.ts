dc5ed7b26ad4e9ea51808681506d91845e95393e
Bryan <bryanstevenal@gmail.com>
2022-12-05 21:10:13 +0100

    Filter endpoint results using HttpParams

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
@@ -11,6 +11,10 @@ export class PaisService {
 
     private apiUrl: string = 'https://restcountries.com/v3.1';
 
+    get httpParams() {
+        return new HttpParams().set('fields', 'flags,name,population,capital,cca2');
+    }
+
     constructor(private http: HttpClient) { }
 
 
@@ -19,7 +23,7 @@ export class PaisService {
 
         console.log("[PaisService] [buscarPais()] url: ", url);
 
-        return this.http.get<Country[]>(url);
+        return this.http.get<Country[]>(url, { params: this.httpParams });
         /*
         .pipe(
             // También podríamos retornar un Observable, p.e. un observable de array vacío
@@ -33,7 +37,7 @@ export class PaisService {
 
         console.log("[PaisService] [buscarCapital()] url: ", url);
 
-        return this.http.get<Country[]>(url);
+        return this.http.get<Country[]>(url, { params: this.httpParams });
 
     }
 
@@ -47,11 +51,16 @@ export class PaisService {
     }
 
     getPaisesPorRegion(region: string): Observable<Country[]> {
-        const url = `${this.apiUrl}/region/${region}`;
+        // const url = `${this.apiUrl}/region/${region}?fields=flags,name,population,capital,cca2`;
+        // const httpParams: HttpParams = new HttpParams().set('fields', 'flags, name, population, capital, cca2');
+        const url = `${this.apiUrl}/region/${region}?`;
 
         console.log("[PaisService] [getPaisesPorRegion()] url: ", url);
 
-        return this.http.get<Country[]>(url);
+        return this.http.get<Country[]>(url, { params: this.httpParams })
+            .pipe(
+                tap(console.log)
+            );
 
     }
 }
